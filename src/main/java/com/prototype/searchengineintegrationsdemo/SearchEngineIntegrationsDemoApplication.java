a27892fdfc0a3c26a6d4dec01c2fc31bf3472c84
Wesley Haagmans <wesley.haagmans@amplexor.com>
2022-03-16 15:44:41 +0100

    +init spring project

---
+++ src/main/java/com/prototype/searchengineintegrationsdemo/SearchEngineIntegrationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.searchengineintegrationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineIntegrationsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineIntegrationsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prototype/searchengineintegrationsdemo/SearchEngineIntegrationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.searchengineintegrationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineIntegrationsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

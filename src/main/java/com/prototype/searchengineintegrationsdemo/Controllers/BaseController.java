833a8996c5c6d37bbf8b387d8d2bba4fff3d3a7b
Wesley Haagmans <wesley.haagmans@amplexor.com>
2022-03-17 17:06:04 +0100

    wip

---
+++ src/main/java/com/prototype/searchengineintegrationsdemo/Controllers/BaseController.java
@@ -0,0 +1,13 @@
+package com.prototype.searchengineintegrationsdemo.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public String index() {
+        return "yeet";
+    }
+}
---
+++ src/main/java/com/prototype/searchengineintegrationsdemo/Models/Author.java
@@ -0,0 +1,11 @@
+package com.prototype.searchengineintegrationsdemo.Models;
+
+import org.springframework.data.annotation.Id;
+
+public class Author {
+
+    @Id
+    private String id;
+
+    private String name;
+}

38a3466cd7c33e879e776252a61bea0f2aba526f
Wesley Haagmans <wesley.haagmans@amplexor.com>
2022-03-23 15:36:09 +0100

    update

--- src/main/java/com/prototype/searchengineintegrationsdemo/Interfaces/ArticleRepository.java
+++ src/main/java/com/prototype/searchengineintegrationsdemo/Interfaces/ArticleRepository.java
@@ -8,9 +8,7 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 public interface ArticleRepository extends ElasticsearchRepository<Article, String> {
 
-//    Page<Article> findByAuthorsName(String name, Pageable pageable);
-
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByAuthorsName(String name, Pageable pageable);
 
     @Query("{\"bool\": {\"must\": [{\"match\": {\"authors.name\": \"?0\"}}]}}")
     Page<Article> findByAuthorsNameUsingCustomQuery(String name, Pageable pageable);
